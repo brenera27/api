33bff714935944ab99f791cc296d1cee64a9519e
breno barbosa <brenobarbosa2705@gmail.com>
2020-08-04 15:03:35 -0300

    cors

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+var cors = require('cors');
+
+
+
 const app = express();
 app.use(express.json());
 app.use('/api', require('./src/routes'));
+app.use(cors());
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
